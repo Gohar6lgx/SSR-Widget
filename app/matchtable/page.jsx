0542f4cfdc4531a34d7cc4079ef747d2b77f7a0d
Muhammad <gohar.butt@sixlogics.com>
2025-05-23 14:58:01 +0500

    new match detail

--- app/matchtable/page.jsx
+++ app/matchtable/page.jsx
@@ -3,7 +3,7 @@ import LoadScript from "../components/LoadScript";
 
 export default async function WidgetPage() {
   const res = await fetch(
-    "http://localhost:3009/loadwidget?dataId=171064&dataType=match-table&sport=football&difference=+5&baseUrl=https://cms.footballfirst.com/api/v1&head=match"
+    "https://ipublisher-microservices.6lgx.com/loadwidget?dataId=171064&dataType=match-table&sport=football&difference=+5&baseUrl=https://cms.footballfirst.com/api/v1&head=match"
   );
   const html = await res.text();
 
