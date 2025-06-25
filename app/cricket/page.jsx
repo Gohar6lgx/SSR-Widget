cdabb633696ac735a00f96021818596f1e6dae9d
Muhammad <gohar.butt@sixlogics.com>
2025-06-25 18:25:03 +0500

    cricket and team pages

---
+++ app/cricket/page.jsx
@@ -0,0 +1,17 @@
+import LoadScript from "../components/LoadScript";
+
+export default async function WidgetPage({}) {
+  const today = new Date().toISOString().split("T")[0]; // Get today's date in YYYY-MM-DD format
+
+  const res = await fetch(
+    `https://ipublisher-microservices.6lgx.com/loadwidget?dataId=171064&dataSSR=ssr&dataDate=${today}&dataType=live-match-detail&sport=cricket&difference=+5&baseUrl=https://cms.cyberstate-tech.com/api/v1&head=match`
+  );
+  const html = await res.text();
+
+  return (
+    <>
+      <LoadScript />
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </>
+  );
+}
